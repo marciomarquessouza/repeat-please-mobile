18bc30d9074ff58804bd0d416c72474e23bf308d
Marcio Souza <marciomultimedia@gmail.com>
2020-01-01 09:45:18 -0300

    fix: scroll option and space between

--- src/screens/register/index.tsx
+++ src/screens/register/index.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import {
 	ButtonRounded,
 	ButtonTransparent,
+	MessageWarning,
 	TitleLogo,
 	PlaceholderInput,
 } from 'repeat-please-styles';
@@ -15,20 +16,39 @@ export class Register extends Component<{}, {}> {
 	render() {
 		return (
 			<View style={styles.container} data-test="register">
-				<TitleLogo />
-				<View style={styles.formStyle}>
-					<PlaceholderInput placeholder="Name or NickName" />
-					<PlaceholderInput placeholder="Email" keyboardType="email-address" />
-					<PlaceholderInput placeholder="Password" secureTextEntry />
-					<PlaceholderInput placeholder="Confirm Password" secureTextEntry />
-				</View>
-				<ButtonRounded customStyle={styles.buttonStyle}>Register</ButtonRounded>
-				<ButtonTransparent customStyle={styles.buttonStyle}>
-					Loging with Facebook
-				</ButtonTransparent>
-				<ButtonTransparent customStyle={styles.buttonStyle}>
-					Loging with Google
-				</ButtonTransparent>
+				<ScrollView contentContainerStyle={styles.scrollStyle}>
+					<View style={styles.wrapper}>
+						<View style={styles.logoContainer}>
+							<TitleLogo />
+						</View>
+						<View style={styles.formStyle}>
+							<PlaceholderInput placeholder="Name or NickName" />
+							<PlaceholderInput
+								placeholder="Email"
+								keyboardType="email-address"
+							/>
+							<PlaceholderInput placeholder="Password" secureTextEntry />
+							<PlaceholderInput
+								placeholder="Confirm Password"
+								secureTextEntry
+							/>
+							<MessageWarning customStyle={styles.messageStyle}>
+								Register error
+							</MessageWarning>
+						</View>
+						<ButtonRounded customStyle={styles.buttonStyle}>
+							Register
+						</ButtonRounded>
+						<View>
+							<ButtonTransparent customStyle={styles.buttonStyle}>
+								Loging with Facebook
+							</ButtonTransparent>
+							<ButtonTransparent customStyle={styles.buttonStyle}>
+								Loging with Google
+							</ButtonTransparent>
+						</View>
+					</View>
+				</ScrollView>
 			</View>
 		);
 	}
--- src/screens/register/styles.ts
+++ src/screens/register/styles.ts
@@ -4,15 +4,27 @@ import { color } from 'repeat-please-styles';
 export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
 		backgroundColor: color.background,
+	},
+	scrollStyle: {
+		flexGrow: 1,
+	},
+	wrapper: {
+		height: '100%',
 		padding: 10,
+		justifyContent: 'space-between',
 	},
 	formStyle: {
 		marginVertical: 20,
 	},
 	buttonStyle: {
 		marginVertical: 10,
+	},
+	messageStyle: {
+		marginVertical: 5,
+	},
+	logoContainer: {
+		marginTop: 10,
+		alignItems: 'center',
 	},
 });
