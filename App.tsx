ad880d6bf10ce976d4c73241c2536adc0b9c73e5
Marcio Marques de Souza <marciomultimedia@gmail.com>
2020-01-06 07:55:58 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,15 +3,7 @@ import SplashScreen from 'react-native-splash-screen';
 import AppContainer from './src/navigator';
 import * as firebase from 'firebase';
 
-const firebaseConfig = {
-	apiKey: "AIzaSyDcMG3Wr5rB8n8hcGvh7kbFisvSx_k4uxk",
-	authDomain: "repeatpleaseapi.firebaseapp.com",
-	databaseURL: "https://repeatpleaseapi.firebaseio.com",
-	projectId: "repeatpleaseapi",
-	storageBucket: "repeatpleaseapi.appspot.com",
-	messagingSenderId: "86626703872",
-	appId: "1:86626703872:web:e1f2ce8753b15482a796e5"
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
