94cd804417f95714bef011d567c4cae480e7ed51
Marcio Souza <marciomultimedia@gmail.com>
2020-01-06 09:55:12 -0300

    fix: firebase config

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import AppContainer from './src/navigator';
 import * as firebase from 'firebase';
+import firebaseConfig from './config.hom';
 
-const firebaseConfig = {};
-
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default class App extends Component {
