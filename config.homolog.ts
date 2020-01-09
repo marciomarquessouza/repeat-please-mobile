e26d15362e2946d5faa6d3858a5d7373318e2723
Marcio Souza <marciomultimedia@gmail.com>
2020-01-08 22:59:12 -0300

    fix: conflicts

--- config.homolog.ts
+++
@@ -1,9 +0,0 @@
-export default {
-	apiKey: "AIzaSyDcMG3Wr5rB8n8hcGvh7kbFisvSx_k4uxk",
-	authDomain: "repeatpleaseapi.firebaseapp.com",
-	databaseURL: "https://repeatpleaseapi.firebaseio.com",
-	projectId: "repeatpleaseapi",
-	storageBucket: "repeatpleaseapi.appspot.com",
-	messagingSenderId: "86626703872",
-	appId: "1:86626703872:web:e1f2ce8753b15482a796e5",
-};
