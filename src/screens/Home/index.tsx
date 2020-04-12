d9fb4e0a2593ac70a5f2ba7fcfb1312cf1d7c58c
Marcio Souza <marciomultimedia@gmail.com>
2020-04-11 21:38:21 -0300

    feat: progress chart

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, SafeAreaView, Text, View, ScrollView } from 'react-native';
 import { SearchBar } from '../../components/SearchBar';
 import { color } from 'repeat-please-styles';
+import { CircularProgress } from 'repeat-please-styles';
 
 export const Home = () => {
+	const [startAnimation, setStartAnimation] = useState(false);
+
+	useEffect(() => {
+		setStartAnimation(true);
+	}, [startAnimation]);
+
 	return (
 		<View style={styles.wrapper}>
 			<SafeAreaView>
 				<SearchBar />
 				<View style={styles.container}>
-					<Text>Welcome Screen</Text>
+					<ScrollView>
+						<View style={styles.chartContainer}>
+							<Text style={styles.sectionTitleStyle}>Your Weekly Progress</Text>
+							<Text style={styles.sectionSubTitleStyle}>22 of 100 Words</Text>
+						</View>
+						<CircularProgress
+							progress={0.75}
+							duration={1000}
+							startAnimation={startAnimation}
+						/>
+					</ScrollView>
 				</View>
 			</SafeAreaView>
 		</View>
@@ -27,5 +44,18 @@ const styles = StyleSheet.create({
 	container: {
 		height: '100%',
 		backgroundColor: '#fff',
+	},
+	chartContainer: {
+		marginTop: 20,
+		marginHorizontal: 20,
+	},
+	sectionTitleStyle: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		lineHeight: 25,
+	},
+	sectionSubTitleStyle: {
+		fontSize: 14,
+		lineHeight: 25,
 	},
 });
