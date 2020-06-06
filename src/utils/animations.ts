75131e4b84b4f288bdef71dac1544393370a759a
Marcio Souza <marciomultimedia@gmail.com>
2020-06-06 16:26:08 -0300

    feat: react native upgrade 0.62.2

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -4,10 +4,10 @@ export const timingAnimation = (
 	component: Animated.Value,
 	toValue: number,
 	duration: number,
-) => Animated.timing(component, { toValue, duration });
+) => Animated.timing(component, { toValue, duration, useNativeDriver: true });
 
 export const springAnimation = (
 	component: Animated.Value,
 	toValue: number,
 	friction: number,
-) => Animated.spring(component, { toValue, friction });
+) => Animated.spring(component, { toValue, friction, useNativeDriver: true });
