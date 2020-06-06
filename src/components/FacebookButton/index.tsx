a43eeda6e673f5b58730f7ee621e960f92908101
Marcio Souza <marciomultimedia@gmail.com>
2020-06-06 16:00:53 -0300

    feat: rn upgrade wip

--- src/components/FacebookButton/index.tsx
+++ src/components/FacebookButton/index.tsx
@@ -1,9 +1,9 @@
-import React, { useContext, useState } from 'react';
-import { AccessToken, LoginManager, LoginResult } from 'react-native-fbsdk';
+import React, { useState } from 'react';
+// import { AccessToken, LoginManager, LoginResult } from 'react-native-fbsdk';
 import { FacebookButton as FacebookBtnComponent } from 'repeat-please-styles';
 import { ViewStyle } from 'react-native';
-import { AlertsContext } from '../../contexts/AlertsContext';
-import { auth } from 'firebase';
+// import { AlertsContext } from '../../contexts/AlertsContext';
+// import { auth } from 'firebase';
 
 interface ILoginProps {
 	children: string;
@@ -14,32 +14,32 @@ export const FacebookButton = ({
 	children,
 	style,
 }: ILoginProps): JSX.Element => {
-	const { showAlert } = useContext(AlertsContext);
-	const [isLoading, setLoading] = useState(false);
+	// const { showAlert } = useContext(AlertsContext);
+	const [isLoading] = useState(false);
 
 	const onPress = () => {
-		setLoading(true);
-		LoginManager.logInWithPermissions(['public_profile', 'email'])
-			.then((result: LoginResult) => {
-				if (result.isCancelled) {
-					throw new Error('Action Canceled');
-				}
-				return AccessToken.getCurrentAccessToken();
-			})
-			.then(data => {
-				if (!data || !data.accessToken) {
-					throw new Error('Facebook server error');
-				}
-				const credential = auth.FacebookAuthProvider.credential(
-					data.accessToken,
-				);
-				auth().signInWithCredential(credential);
-			})
-			.catch(({ message: facebookError }) => {
-				setLoading(false);
-				const message = facebookError || 'Login Error';
-				showAlert({ message, type: 'error' });
-			});
+		// setLoading(true);
+		// LoginManager.logInWithPermissions(['public_profile', 'email'])
+		// 	.then((result: LoginResult) => {
+		// 		if (result.isCancelled) {
+		// 			throw new Error('Action Canceled');
+		// 		}
+		// 		return AccessToken.getCurrentAccessToken();
+		// 	})
+		// 	.then(data => {
+		// 		if (!data || !data.accessToken) {
+		// 			throw new Error('Facebook server error');
+		// 		}
+		// 		const credential = auth.FacebookAuthProvider.credential(
+		// 			data.accessToken,
+		// 		);
+		// 		auth().signInWithCredential(credential);
+		// 	})
+		// 	.catch(({ message: facebookError }) => {
+		// 		setLoading(false);
+		// 		const message = facebookError || 'Login Error';
+		// 		showAlert({ message, type: 'error' });
+		// 	});
 	};
 
 	return (
--- src/components/GoogleButton/index.tsx
+++ src/components/GoogleButton/index.tsx
@@ -1,10 +1,10 @@
-import React, { useContext, useState } from 'react';
-import { GoogleSignin } from 'react-native-google-signin';
-import { googleLoginConfig } from '../../../config.homolog';
+import React, { useState } from 'react';
+// import { GoogleSignin } from 'react-native-google-signin';
+// import { googleLoginConfig } from '../../../config.homolog';
 import { GoogleButton as GoogleBtnComponent } from 'repeat-please-styles';
-import { AlertsContext } from '../../contexts/AlertsContext';
+// import { AlertsContext } from '../../contexts/AlertsContext';
 import { ViewStyle } from 'react-native';
-import { auth } from 'firebase';
+// import { auth } from 'firebase';
 
 interface ILoginProps {
 	children: string;
@@ -12,24 +12,23 @@ interface ILoginProps {
 }
 
 export const GoogleButton = ({ children, style }: ILoginProps): JSX.Element => {
-	const { showAlert } = useContext(AlertsContext);
-	const [isLoading, setLoading] = useState(false);
+	const [isLoading] = useState(false);
 
 	const onPress = async () => {
-		try {
-			setLoading(true);
-			await GoogleSignin.configure(googleLoginConfig);
-			const data = await GoogleSignin.signIn();
-			const credential = auth.GoogleAuthProvider.credential(
-				data.idToken,
-				data.serverAuthCode,
-			);
-			await auth().signInWithCredential(credential);
-		} catch ({ code }) {
-			setLoading(false);
-			const message = code === '-5' ? 'Action Canceled' : 'Login Error';
-			showAlert({ message, type: 'error' });
-		}
+		// try {
+		// 	setLoading(true);
+		// 	await GoogleSignin.configure(googleLoginConfig);
+		// 	const data = await GoogleSignin.signIn();
+		// 	const credential = auth.GoogleAuthProvider.credential(
+		// 		data.idToken,
+		// 		data.serverAuthCode,
+		// 	);
+		// 	await auth().signInWithCredential(credential);
+		// } catch ({ code }) {
+		// 	setLoading(false);
+		// 	const message = code === '-5' ? 'Action Canceled' : 'Login Error';
+		// 	showAlert({ message, type: 'error' });
+		// }
 	};
 
 	return (
