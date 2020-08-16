71d157e3b7fc2b9bae188ea4daa3e898ab3ffcc4
Marcio Souza <marciomultimedia@gmail.com>
2020-08-16 19:12:44 -0300

    feat: removing useless loading

--- src/reducers/profileReducer.ts
+++ src/reducers/profileReducer.ts
@@ -4,13 +4,11 @@ import { IProfile } from '../../types/profile';
 interface IProfileState {
 	profile: IProfile | null;
 	error: string;
-	isLoading: boolean;
 }
 
 const initialState: IProfileState = {
 	profile: null,
 	error: '',
-	isLoading: false,
 };
 
 export const profileReducer = (
@@ -18,30 +16,20 @@ export const profileReducer = (
 	action: actions.ProfileAction,
 ): IProfileState => {
 	switch (action.type) {
-		case actions.PROFILE_SET_REQUEST:
-		case actions.PROFILE_GET_REQUEST:
-		case actions.PROFILE_CLEAR_REQUEST:
-			return {
-				...state,
-				isLoading: true,
-			};
 		case actions.PROFILE_SET_SUCCESS:
 		case actions.PROFILE_GET_SUCCESS:
 			return {
 				...state,
 				profile: action.profile,
-				isLoading: false,
 			};
 		case actions.PROFILE_CLEAR_SUCCESS:
 			return {
 				...state,
 				profile: null,
-				isLoading: false,
 			};
 		case actions.PROFILE_ERROR:
 			return {
 				...state,
-				isLoading: false,
 				error: action.error,
 			};
 		default:
